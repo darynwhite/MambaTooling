7adaff678803019f56cda323ff0e06df845904e0
Daryn White <daryn.white@noaa.gov>
2024-09-26 14:27:38 -0700

    reconfigure, update, and test

--- condaFromScratch.sh
+++ condaFromScratch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-envInstall=$DIR/lib/conda_env_setup.sh
-envs=$DIR/envs/
+envInstall="$DIR"/lib/conda_env_setup.sh
+envs="$DIR"/envs/
 
-source $DIR/lib/conda_build.sh
+source "$DIR"/lib/conda_build.sh
 mamba init zsh
 mamba update -a -y
 mamba update -y conda
-source ${envInstall} ${envs}
+# shellcheck source=./lib/conda_env_setup.sh
+source "${envInstall}" "${envs}"
--- lib/conda_build.sh
+++ lib/conda_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-source $DIR/setup.sh
+source "$DIR"/lib/setup.sh
 # conda=Miniconda3-latest-MacOSX-arm64.sh
-conda=Mambaforge-MacOSX-arm64.sh
 # curl -O "https://repo.anaconda.com/miniconda/${conda}"
+conda=Mambaforge-MacOSX-arm64.sh
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/${conda}"
-bash ${conda} -b -p ${HOME}/conda
-SetFile -a V ${HOME}/conda
+bash ${conda} -b -p "${HOME}"/conda
+SetFile -a V "${HOME}"/conda
