1986f56546c6cb6ac3b590575c7b999ae7cf812e
Daryn <darynw@uw.edu>
2025-07-02 15:00:44 -0700

    Update conda_build.sh
    No platform specifics.

--- lib/conda_build.sh
+++ lib/conda_build.sh
@@ -5,6 +5,6 @@ source "$DIR"/setup.sh
 ## Swqap comments for using MiniConda over MiniForge/Mamba
 # conda=Miniconda3-latest-MacOSX-arm64.sh
 # curl -O "https://repo.anaconda.com/miniconda/${conda}"
-curl -fsSLo Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-$(uname -m).sh"
+curl -fsSLo Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3.sh -b -p "${HOME}"/conda
 SetFile -a V "${HOME}"/conda
